b21e39f9b85ff379bce79c7eb0ddd7234b3d926f
khushi-sahani <khushisahani957@gmail.com>
2026-03-05 14:36:19 +0530

    Added calculator project

---
+++ calculator.py
@@ -0,0 +1,36 @@
+print("-----Simple Calculator-----")
+print("1 - Add")
+print("2 - Subtract")
+print("3 - Multiply")
+print("4 - Divide")
+
+option = int(input("Choose an operation: "))
+
+if option in [1, 2, 3, 4]:
+
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+
+    if option == 1:
+        result = num1 + num2
+
+    elif option == 2:
+        result = num1 - num2
+
+    elif option == 3:
+        result = num1 * num2
+
+    elif option == 4:
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            print("Cannot divide by zero")
+            result = None
+
+    if result is not None:
+        print("The result of the operation is {}".format(result))
+
+else:
+    print("Invalid operation entered")
+
+    print("Thankyou for using calculator")
